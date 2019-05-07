e54be08288b8761cd9233dbc5bb15c9f117c5e85
Krystian Gulinski <gulinski.krystian@gmail.com>
2019-05-07 20:48:21 +0200

    revert lazy version

--- src/Cube.jl
+++ src/Cube.jl
@@ -173,6 +173,80 @@ function render!(output::Array{UInt8, 2}, cube::Cube_julia)
     output
 end # function
 
+function render_lazy!(output::Array{UInt8, 2}, cube::DrawnObject)
+    # construct rotation matrix
+
+    Rz = Matrix{Float64}(I, 4, 4)
+    Rz[1, 1] = cos(cube.rotation_vector[3])
+    Rz[1, 2] = sin(cube.rotation_vector[3])
+    Rz[2, 1] = -sin(cube.rotation_vector[3])
+    Rz[2, 2] = cos(cube.rotation_vector[3])
+
+    Ry = Matrix{Float64}(I, 4, 4)
+    Ry[1, 1] = cos(cube.rotation_vector[2])
+    Ry[3, 1] = sin(cube.rotation_vector[2])
+    Ry[1, 3] = -sin(cube.rotation_vector[2])
+    Ry[3, 3] = cos(cube.rotation_vector[2])
+
+    Rx = Matrix{Float64}(I, 4, 4)
+    Rx[2, 2] = cos(cube.rotation_vector[1])
+    Rx[2, 3] = sin(cube.rotation_vector[1])
+    Rx[3, 2] = -sin(cube.rotation_vector[1])
+    Rx[3, 3] = cos(cube.rotation_vector[1])
+
+    R = Rz * Ry * Rx
+
+    @inbounds R[1:3,4] = cube.position_vector
+
+    @inbounds vertices = map(cube.vertices) do x
+        R * x.position_vector
+    end
+
+    distance = -100
+    half_size = 256
+
+    @inbounds cords = map(vertices) do x
+        (x .* (distance / x[3]) .+ half_size )
+    end
+
+    # line drawing
+
+    for c in cube.connections
+        from = cords[c.from + 1]
+        to = cords[c.to + 1]
+
+        x1 = from[1]
+        y1 = from[2]
+
+        x2 = to[1]
+        y2 = to[2]
+
+        dx = x2 - x1
+        dy = y2 - y1
+
+        step = abs(dx) >= abs(dy) ? abs(dx) : abs(dy)
+
+        dx = dx / step
+        dy = dy / step
+
+        x = x1
+        y = y1
+        i = 1.0
+
+        while i <= step
+            xcord = trunc(Int32, x) * 3
+            ycord = trunc(Int32, y)
+
+            output[xcord:xcord+2, ycord] .= 0xFF
+
+            x += dx
+            y += dy
+            i += 1.0
+        end
+    end
+    output
+end
+
 function ccall_simple()
     ccall((:simple_fun, "src/simple.lib"), Int32, (Int32,), 10)
 
